03a6324515fa84ad1aad6f03c8d58909e713a97e
Michael OBrien <michael@obrienlabs.org>
2019-06-07 11:11:07 -0400

    #2 add root pom project

--- difference-nbi/src/main/docker/startService.sh
+++ difference-nbi/src/main/docker/startService.sh
@@ -3,7 +3,7 @@
 cd /opt/app
 #-Dlogging.config=config/logback.xml
 if [ -z "${java_runtime_arguments}" ]; then
-java  -Xms128m -Xmx512m -jar /opt/app/lib/difference-nbi-0.0.1-SNAPSHOT.jar
+java  -Xms128m -Xmx512m -jar /opt/app/lib/difference-nbi-0.0.2-SNAPSHOT.jar
 else
-java  $java_runtime_arguments -jar /opt/app/lib/difference-nbi-0.0.1-SNAPSHOT.jar
+java  $java_runtime_arguments -jar /opt/app/lib/difference-nbi-0.0.2-SNAPSHOT.jar
 fi
